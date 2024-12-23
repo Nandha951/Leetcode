cc42b86f49f286d32bc37c0941bd059e9c59d573
Nandhakumar <40836847+Nandha951@users.noreply.github.com>
2024-12-22 16:08:35 -0800

    1. Two Sum - Optimal.py

--- Array/1. Two Sum - Optimal.py
+++ Array/1. Two Sum - Optimal.py
@@ -1,8 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in hashmap:
-                return [hashmap[complement], i]
-            hashmap[num] = i
+        if len(nums) < 2:
+            return []
+
+        arr_with_indices = [(num, i) for i, num in enumerate(nums)]
+        arr_with_indices.sort()
+
+        left, right = 0, len(arr_with_indices) - 1
+        while left < right:
+            current_sum = arr_with_indices[left][0] + arr_with_indices[right][0]
+            if current_sum == target:
+                return [arr_with_indices[left][1], arr_with_indices[right][1]]
+            elif current_sum < target:
+                left+=1
+            elif current_sum > target:
+                right-=1
