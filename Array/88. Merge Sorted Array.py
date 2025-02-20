2e26f09de95b24e554619e58331875901ebfac54
Nandhakumar <40836847+Nandha951@users.noreply.github.com>
2025-02-20 14:44:33 -0800

    Merge sorted arrary - added edge cases

--- Array/88. Merge Sorted Array.py
+++ Array/88. Merge Sorted Array.py
@@ -22,4 +22,7 @@ class Solution:
         return nums1
 
 sol = Solution()
-print(sol.merge([1,2,3,0,0,0], 3, [2,5,6], 3)) # [1,2,2,3,5,6]
+# print(sol.merge([1,2,3,0,0,0], 3, [2,5,6], 3)) # [1,2,2,3,5,6]
+# print(sol.merge([1], 1, [], 0)) # [1]
+# print(sol.merge([0], 0, [1], 1)) # [1]
+print(sol.merge([2,0], 1, [1], 1)) # [1,2]
